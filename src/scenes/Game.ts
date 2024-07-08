6c67ea81634260fbd5eb8b1be6be560e96ba63d7
Juergen Hoedl <Juergen.Hoedl@itk-engineering.de>
2024-07-08 18:47:21 +0200

    basic project setup (typescript, phaser, vite)

---
+++ src/scenes/Game.ts
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+
+export default class Demo extends Phaser.Scene {
+  constructor() {
+    super('GameScene');
+  }
+
+  preload() {
+
+  }
+
+  create() {
+
+  }
+
+  update(time: number, delta: number): void {
+      
+  }
+}
