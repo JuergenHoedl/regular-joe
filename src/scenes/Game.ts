3e71b3a4f8242ba639420fec3e7b6ef147f02c65
Juergen Hoedl <Juergen.Hoedl@itk-engineering.de>
2024-07-09 13:06:15 +0200

    Add a NPS sprite. If player comes near a "Talk" option will appear

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,10 +2,17 @@ import Phaser from 'phaser';
 
 export default class Demo extends Phaser.Scene {
   player!: Phaser.Physics.Arcade.Sprite;
+  uncle!: Phaser.Physics.Arcade.Sprite;
+
   upKey!: Phaser.Input.Keyboard.Key;
   downKey!: Phaser.Input.Keyboard.Key;
   leftKey!: Phaser.Input.Keyboard.Key;
   rightKey!: Phaser.Input.Keyboard.Key;
+
+  playerNearUncle: boolean = false;
+
+  playerActionOptions: Phaser.GameObjects.Text[] = [];
+
 
   constructor() {
     super('GameScene');
@@ -13,9 +20,12 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.image('player', 'assets/player.png');
+    this.load.image('npc', 'assets/npc.png');
   }
 
   create() {
+    this.uncle = this.physics.add.sprite(200, 200, 'npc');
+
     this.player = this.physics.add.sprite(400, 300, 'player');
 
     this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -38,6 +48,40 @@ export default class Demo extends Phaser.Scene {
     } else if (this.downKey.isDown && !this.upKey.isDown) {
       this.player.setVelocityY(200);
     }
+
+
+    if ( this.calcDistance(this.player, this.uncle) < 50 ){
+      if (!this.playerNearUncle) {
+        this.playerNearUncle = true;
+        this.addActionOption(10, 10, 'Talk');
+      }
+    } else {
+      if (this.playerNearUncle) {
+        this.playerNearUncle = false;
+        this.removeActionOption(0);
+      }
+    }
       
   }
+
+  calcDistance(obj1: Phaser.Physics.Arcade.Sprite, obj2: Phaser.Physics.Arcade.Sprite) {
+    return Phaser.Math.Distance.Between(obj1.x, obj1.y, obj2.x, obj2.y);
+  }
+
+  addActionOption(x: number, y: number, text: string) {
+    let button = this.add.text(x, y, text)
+      .setInteractive()
+      .on('pointerdown', () => console.log(`Button ${text} clicked`));
+  
+    this.playerActionOptions.push(button);
+  }
+
+  removeActionOption(index: number) {
+    if (index >= 0 && index < this.playerActionOptions.length) {
+      this.playerActionOptions[index].destroy();
+      this.playerActionOptions.splice(index, 1);
+    }
+  }
+
+
 }
